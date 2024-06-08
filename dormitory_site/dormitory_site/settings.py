38fdb50999dc0d12f36d1cacbc18a61db42660a2
Mixail Maksimov <maks.misha2001@gmail.com>
2024-06-08 16:47:40 +0300

    apps

--- dormitory_site/dormitory_site/settings.py
+++ dormitory_site/dormitory_site/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_page',
     'personal_account',
+    'invite_guest',
+    'notifications',
+    'payment',
+    'troubleshooting',
 ]
 
 MIDDLEWARE = [
